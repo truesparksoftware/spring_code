b31761f8692557c2670cde5b47adf43ef6b5998e
truesparksoftware@gmail.com <jeevanbarguje1@gmail.com>
2022-05-23 21:13:08 +0530

    removed getter setter as business requiremnt

--- springboot-web-thymeleaf/src/main/java/com/hehe/entity/User.java
+++ springboot-web-thymeleaf/src/main/java/com/hehe/entity/User.java
@@ -12,27 +12,5 @@ public class User {
         this.password=password;
     }
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  
 }
